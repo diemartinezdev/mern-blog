5f008e1297d49eaaf2c23378505026bcbb47f7af
Diego Martinez <martinez.diego90@gmail.com>
2023-05-24 22:00:25 +0200

    trying connection with mongo

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+const articlesInfo = {
+  "learn-react": {
+    comments: [],
+  },
+  "learn-node": {
+    comments: [],
+  },
+  "my-thoughts-on-learning-react": {
+    comments: [],
+  },
+};
+
+// Initialize middleware
+app.use(express.json({ extended: false }));
+
+app.post("/api/articles/:name/add-comments", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
