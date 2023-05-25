3fcc94f4bfa2ce0b688dbf9f4cd3ce3a696b3eab
Diego Martinez <martinez.diego90@gmail.com>
2023-05-25 10:57:20 +0200

    keep trying to connect to db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { MongoClient } = require("mongodb")
 const PORT = process.env.PORT || 8000;
 
 const articlesInfo = {
@@ -16,6 +17,19 @@ const articlesInfo = {
 
 // Initialize middleware
 app.use(express.json({ extended: false }));
+
+app.get('/api/article/:name', async (req, res) => {
+  try {
+    const articleName = req.params.name;
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db("mernblog");
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "error connecting to database", error });
+  }
+});
 
 app.post("/api/articles/:name/add-comments", (req, res) => {
   const { username, text } = req.body;
